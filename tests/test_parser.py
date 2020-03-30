c90bbb7a912aa40f8eaddb3daf8147fa549c9e37
Dmitrij Kultasev <dkultasev@gmail.com>
2020-03-30 20:50:58 +0300

    Rename unit tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -9,7 +9,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 # excluding following line for linter as it complains that
 # from import is supposed to be at the top of the file
-from src.parse import (get_page_content, validate_title) # noqa
+from src.parse import (get_page_content, validate_title)  # noqa
 
 
 class TestsGetPage(unittest.TestCase):
@@ -20,14 +20,15 @@ class TestsGetPage(unittest.TestCase):
         pass
 
     @patch('requests.get')
-    def test_right_url(self, mock_request):
+    def test_right_url_is_passed_to_reponse_get_is(self, mock_request):
         mock_request.return_value.status_code = 200
         exepted_url = 'https://smarsy.ua/'
         get_page_content(exepted_url)
         mock_request.assert_called_with(exepted_url)
 
     @patch('requests.get')
-    def test_response_status_code_ok(self, mock_response):
+    def test_response_with_status_200_returns_expected_text(self,
+                                                            mock_response):
         url = 'https://smarsy.ua/'
         mock_response.return_value.status_code = 200
         expected_text = 'This is login Page'
@@ -35,12 +36,13 @@ class TestsGetPage(unittest.TestCase):
         self.assertEqual(get_page_content(url), expected_text)
 
     @patch('requests.get')
-    def test_response_status_code_404(self, mock_response):
+    def test_response_with_status_code_404_raises_exception(self,
+                                                            mock_response):
         url = 'https://smarsy.ua/'
         mock_response.return_value.status_code = 404
         self.assertRaises(requests.HTTPError, get_page_content, url)
 
-    def test_page_title_validate(self):
+    def test_login_page_has_expected_title(self):
         html = '<html><title>Smarsy - Смарсі - Україна</title></html>'
         actual = validate_title(html)
         self.assertTrue(actual)
@@ -48,4 +50,4 @@ class TestsGetPage(unittest.TestCase):
 
 if __name__ == '__main__':
     if '--unittest' in sys.argv:
-        subprocess.call([sys.executable, '-m', 'unittest', 'discover']) 
+        subprocess.call([sys.executable, '-m', 'unittest', 'discover'])
