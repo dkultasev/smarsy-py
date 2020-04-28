ca84c3e2b1917aadf682dbd69566ecc01259743a
MaxxxZu <zulcle.m@gmail.com>
2020-04-28 11:41:07 +0300

    Deleted setUp in TestBSHelperInstance

--- tests/test_bs_helper.py
+++ tests/test_bs_helper.py
@@ -15,12 +15,10 @@ from smarsy.bs_helper import BSHelper # noqa
 
 
 class TestBSHelperInstance(unittest.TestCase):
-    def setUp(self):
-        self.html = 'some html'
-        self.source_page = BSHelper(self.html)
-
     def test_bshelper_instance_created(self):
-        self.assertEqual(self.source_page.html, self.html)
+        html = 'some html'
+        source_page = BSHelper(html)
+        self.assertEqual(source_page.html, html)
 
 
 class TestGetPageSource(unittest.TestCase):
